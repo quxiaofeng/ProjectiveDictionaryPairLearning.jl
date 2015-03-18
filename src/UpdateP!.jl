aaef7953b7e75e6658a33f76d2ece8c57f38e5ae
Xiaofeng QU <xiaofeng.qu.hk@ieee.org>
2015-03-18 14:08:49 +0800

    Prepared to fix the capital file names by moving the files out of the git
    Signed-off-by: Xiaofeng QU <xiaofeng.qu.hk@ieee.org>

--- src/UpdateP!.jl
+++
@@ -1,9 +0,0 @@
-function updateP!(P, A, DataInvMat, DataMat, τ)
-# Update P by Eq. (10)
-
-    for i=1:length(P)
-        @inbounds tempMat = A[i] * DataMat[i]'
-        scale!(tempMat, τ)
-        @inbounds P[i] = tempMat * DataInvMat[i]
-    end
-end
