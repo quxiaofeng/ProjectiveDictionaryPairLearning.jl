6a7917d9794e4e0b597490f6b5483087e3f96b8e
quxiaofeng <xiaofeng.qu.hk@ieee.org>
2015-12-14 12:53:46 +0800

    Fixing the normcol_lesseual.jl by transpose the output matrix (Is it right?)
    Signed-off-by: quxiaofeng <xiaofeng.qu.hk@ieee.org>

--- src/normcol_lessequal.jl
+++ src/normcol_lessequal.jl
@@ -2,4 +2,5 @@
 # matout = argmin||matout-matin||_F^2, s.t. matout(:,i)<=1
 function normcol_lessequal(matin::Matrix{Float64})
     broadcast(/, matin, max(1.0, sqrt(sum(abs2(matin), 2))))
+    matin = matin'
 end
