cb60882e05a5d29e5631a1e3a9fb8a8f202d9648
quxiaofeng <xiaofeng.qu.hk@ieee.org>
2015-12-14 13:13:45 +0800

    Fixed the normcol_lessequal.jl by chaning the sum dim
    Signed-off-by: quxiaofeng <xiaofeng.qu.hk@ieee.org>

--- src/normcol_lessequal.jl
+++ src/normcol_lessequal.jl
@@ -1,6 +1,5 @@
 # solve the proximal problem
 # matout = argmin||matout-matin||_F^2, s.t. matout(:,i)<=1
 function normcol_lessequal(matin::Matrix{Float64})
-    broadcast(/, matin, max(1.0, sqrt(sum(abs2(matin), 2))))
-    matin = matin'
+    broadcast(/, matin, max(1.0, sqrt(sum(abs2(matin), 1))))
 end
