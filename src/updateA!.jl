efaa537905d2f7782cbc44ba92552b1ac45315c7
Xiaofeng QU <xiaofeng.qu.hk@ieee.org>
2016-04-26 19:26:41 +0800

    Updated an iteration loop
    Signed-off-by: Xiaofeng QU <xiaofeng.qu.hk@ieee.org>

--- src/updateA!.jl
+++ src/updateA!.jl
@@ -3,8 +3,8 @@ include("diagadd!.jl")
 function updateA!(A::Array{Any,1}, D::Array{Any,1}, DataMat::Array{Any,1}, P::Array{Any,1}, τ::Float64, DictSize::Int64)
     # Update tempDictCoef by Eq. (8)
 
-    for i=1:length(A)
-        @inbounds TempDict::Matrix{Float64} = D[i]
+    for (i, Dᵢ) in enumerate(D)
+        @inbounds TempDict::Matrix{Float64} = Dᵢ
         @inbounds TempData::Matrix{Float64} = DataMat[i]
         tempDictCoef::Matrix{Float64}       = TempDict' * TempDict
         tempDictDataCoef::Matrix{Float64}   = TempDict' * TempData
